f74dbc4ad67b0af7cea25fd9998c76eb6ef56a91
Kienen Mason <kienen.mason@gmail.com>
2016-03-02 13:46:05 -0500

    Create an event in Google Calendar

--- event/calendar.py
+++ event/calendar.py
@@ -8,11 +8,26 @@ from oauth2client import tools
 import datetime
 from django.http import HttpResponse
 
+from django.forms.models import model_to_dict
+import json
+from event.models import Event
+
 from oauth2client.service_account import ServiceAccountCredentials
 import argparse
 CLIENT_SECRET_FILE = os.path.join(os.path.dirname(__file__), '..', 'client_secret.json')
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 APPLICATION_NAME = 'SD Burner Events'
+
+import datetime
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime.datetime):
+        serial = obj.isoformat()
+        dict = {'dateTime': serial, 'timeZone': 'America/Los_Angeles'}
+        return dict
+    raise TypeError ("Type not serializable")
 
 def get_10_events():
     """Shows basic usage of the Google Calendar API.
@@ -37,6 +52,28 @@ def get_10_events():
     response = HttpResponse()
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        response.write(start, event['summary'])
+        response.write( "%s - %s" % (start , event['summary']))
 
     return response
+
+def add_event(event):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CLIENT_SECRET_FILE, scopes=SCOPES)
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    event_dict= model_to_dict(event, exclude=['owner', 'approved', 'id'])
+
+    #These should be changed and model updated###
+    event_dict['summary']= event_dict.pop('title')
+    event_dict['start']= event_dict.pop('date')
+    event_dict['end']= event_dict['start']+ datetime.timedelta(hours=1)
+    ######
+
+    event_json= json.dumps(event_dict, default=json_serial)
+    event_obj= json.loads(event_json)
+    
+    print(event_json)
+    #httplib2.debuglevel = 4
+    cal_event = service.events().insert(calendarId='primary', body=event_obj).execute()
+    print ('Event created: %s' % (cal_event.get('htmlLink')))
--- event/signals.py
+++ event/signals.py
@@ -1,14 +1,21 @@
 from django.db.models.signals import post_save
-from django.core.signals import request_finished
+#from django.core.signals import request_finished
 from django.dispatch import receiver
+from django.core import serializers
+from django.forms.models import model_to_dict
+import json
 from event.models import Event
+from event import calendar
+
+
 
 @receiver(post_save, sender=Event)
 def saved_event(sender, **kwargs):
     for key, value in kwargs.items():
         print( "%s = %s" % (key, value))
-    print (kwargs['instance'].approved)
+    print (kwargs['instance'].title)
 
-# @receiver(request_finished)
-# def my_callback(sender, **kwargs):
-#     print("Request finished!")
+    event= kwargs['instance']
+    if True: #event.approved:
+        calendar.add_event(event)
+
