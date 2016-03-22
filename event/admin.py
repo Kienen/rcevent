c22d2b7dfd01b417116bc75c3af53330cc0f53fa
Your Name <kienen.mason@gmail.com>
2016-03-21 18:25:33 -0700

    Limit to one newsletter object

--- event/admin.py
+++ event/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from event.models import *
 
 # Register your models here.
+class NewsletterAdmin(admin.ModelAdmin):
+  def has_add_permission(self, request):
+    num_objects = self.model.objects.count()
+    if num_objects >= 1:
+      return False
+    else:
+      return True
+
 admin.site.register(Event)
 admin.site.register(Calendar)
-admin.site.register(Newsletter)
+admin.site.register(Newsletter, NewsletterAdmin)
